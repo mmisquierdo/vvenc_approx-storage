639b5c202d2b76367e4ffb8f23855708ad22d9a2
mmisquierdo <mmisquierdo@inf.ufpel.edu.br>
2026-04-18 02:21:14 -0700

    proper fix to dummy instrumentation function grouping

--- source/Lib/CommonLib/approx.cpp
+++ source/Lib/CommonLib/approx.cpp
@@ -1,45 +1,44 @@
 #include "approx.h"
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::start_level(int64_t level/* = 0*/){ // 1 parametro
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::start_level(int64_t level/* = 0*/){ // 1 parameters
 	return 0;
 }
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::end_level(){					// 0 parameters
-	return 0;
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::end_level(){ // 0 parameters
+	return 1;
 }
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::add_approx(void * const start_address, void const * const end_address, const int64_t bufferId, const int64_t configurationId, const uint64_t dataSizeInBytes, const int64_t isPrecise /*= false*/) //6 parameters
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::add_approx(void * const start_address, void const * const end_address, const int64_t bufferId, const int64_t configurationId, const size_t dataSizeInBytes, const bool isPrecise /*= false*/) //6 parameters
 {
-	return 0;
+	return 2;
 }
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::remove_approx(void * const start_address, void const * const end_address, const bool giveAwayRecords/*= true*/)	//3 parameters
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::remove_approx(void * const start_address, void const * const end_address, const bool giveAwayRecords/*= true*/)	//3 parameters
 {
-	return 0;
+	return 3;
 }
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::next_period(const int64_t isLinearIncrement /*= true*/, const int64_t orNewSetValue /*= std::numeric_limits<int64_t>::min()*/)	// 2 parameters
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::next_period(const bool isLinearIncrement /*= true*/, const int64_t orNewSetValue /*= std::numeric_limits<int64_t>::min()*/)	// 2 parameters
 {
-	return 0;
+	return 4;
 }
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::enable_global_injection(int64_t a1/* = 0*/, int64_t a2/* = 0*/, int64_t a3/* = 0*/, int64_t a4/* = 0*/) //4 parameters
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::enable_global_injection() // 0 parameters
 {
-	return 0;
+	return 5;
 }
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::disable_global_injection(int64_t a1/* = 0*/, int64_t a2/* = 0*/, int64_t a3/* = 0*/, int64_t a4/* = 0*/, int64_t a5/* = 0*/) //5 parameters
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::disable_global_injection() // 0 parameters
 {
-	return 0;
+	return 6;
 }
 
-int64_t __attribute__((optimize("O0"))) ApproxSS::disable_access_instrumentation(int64_t a1/* = 0*/, int64_t a2/* = 0*/, int64_t a3 /* = 0*/, int64_t a4 /* = 0*/, int64_t a5 /* = 0*/, int64_t a6 /* = 0*/, int64_t a7 /* = 0*/) //7 parameters
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::disable_access_instrumentation() // 0 parameters
 {
-	return 0;
+	return 7;
 }
 
-
-int64_t __attribute__((optimize("O0"))) ApproxSS::enable_access_instrumentation(int64_t a1 /* = 0*/, int64_t a2 /* = 0*/, int64_t a3 /* = 0*/, int64_t a4 /* = 0*/, int64_t a5 /* = 0*/, int64_t a6 /* = 0*/, int64_t a7 /* = 0*/, int64_t a8 /* = 0*/) //8 parameters
+uint8_t __attribute__((noinline, optimize("O0"))) ApproxSS::enable_access_instrumentation() // 0 parameters
 {
-	return 0;
+	return 8;
 }
--- source/Lib/CommonLib/approx.h
+++ source/Lib/CommonLib/approx.h
@@ -1,29 +1,28 @@
 #ifndef APPROX_INSTRUMENTATION_H
 #define APPROX_INSTRUMENTATION_H
 
-//parameters named with a single letter are not actually used, they just serve to avoid a Pin bug.
-
 #include <cstdint>
 #include <limits>
+#include <string>
 
 namespace ApproxSS {
-	int64_t __attribute__((optimize("O0"))) start_level(const int64_t level = 0); //1 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) start_level(const int64_t level = 0); //1 parameters
 
-	int64_t __attribute__((optimize("O0"))) end_level(); //0 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) end_level(); //0 parameters
 
-	int64_t __attribute__((optimize("O0"))) add_approx(void * const start_address, void const * const end_address, const int64_t bufferId, const int64_t configurationId, const uint64_t dataSizeInBytes, const int64_t isPrecise = false); //6 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) add_approx(void * const start_address, void const * const end_address, const int64_t bufferId, const int64_t configurationId, const size_t dataSizeInBytes, const bool isPrecise = false); //6 parameters
 
-	int64_t __attribute__((optimize("O0"))) remove_approx(void * const start_address, void const * const end_address, const bool giveAwayRecords = true); //3 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) remove_approx(void * const start_address, void const * const end_address, const bool giveAwayRecords = true); //3 parameters
 
-	int64_t __attribute__((optimize("O0"))) next_period(const int64_t isLinearIncrement = true, const int64_t orNewSetValue = std::numeric_limits<int64_t>::min()); // 2 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) next_period(const bool isLinearIncrement = true, const int64_t orNewSetValue = std::numeric_limits<int64_t>::min()); // 2 parameters
 
-	int64_t __attribute__((optimize("O0"))) enable_global_injection(int64_t a1 = 0, int64_t a2 = 0, int64_t a3 = 0, int64_t a4 = 0); //4 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) enable_global_injection(); //0 parameters
 
-	int64_t __attribute__((optimize("O0"))) disable_global_injection(int64_t a1 = 0, int64_t a2 = 0, int64_t a3 = 0, int64_t a4 = 0, int64_t a5 = 0); //5 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) disable_global_injection(); //0 parameters
 
-	int64_t __attribute__((optimize("O0"))) disable_access_instrumentation(int64_t a1 = 0, int64_t a2 = 0, int64_t a3 = 0, int64_t a4 = 0, int64_t a5 = 0, int64_t a6 = 0, int64_t a7 = 0); //7 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) disable_access_instrumentation(); //0 parameters
 
-	int64_t __attribute__((optimize("O0"))) enable_access_instrumentation(int64_t a1 = 0, int64_t a2 = 0, int64_t a3 = 0, int64_t a4 = 0, int64_t a5 = 0, int64_t a6 = 0, int64_t a7 = 0, int64_t a8 = 0); //8 parameters
+	uint8_t __attribute__((noinline, optimize("O0"))) enable_access_instrumentation(); //0 parameters
 }
 
 #endif /* APPROX_INSTRUMENTATION_H */
